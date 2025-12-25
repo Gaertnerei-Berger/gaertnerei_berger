dd2e0d0104bf4149908c94e021a374b5288d9ae6
enerschnutz <ich@gaertnerei-berger.de>
2025-12-25 08:50:28 +0100

    first commit

---
+++ suppliercatalog/suppliercatalog/doctype/item_bnn/test_item_bnn.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Gärtnerei Berger and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestItemBNN(FrappeTestCase):
+	pass
