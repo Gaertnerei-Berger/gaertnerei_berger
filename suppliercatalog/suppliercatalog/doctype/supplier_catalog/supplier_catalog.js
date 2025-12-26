fa5eb83aecfd1e22c34130231264b245f5f58f1b
enerschnutz <ich@gaertnerei-berger.de>
2025-12-26 05:18:19 +0100

    import works with spezial charakters

--- suppliercatalog/suppliercatalog/doctype/supplier_catalog/supplier_catalog.js
+++ suppliercatalog/suppliercatalog/doctype/supplier_catalog/supplier_catalog.js
@@ -1,9 +1,23 @@
 // Copyright (c) 2025, Gärtnerei Berger and contributors
 // For license information, please see license.txt
+frappe.ui.form.on("Supplier Catalog", {
+  refresh(frm) {
+    // Button nur bei gespeicherten Docs (optional)
+    if (!frm.is_new()) {
+      frm.add_custom_button(__("Start Import"), () => {
+        frm.trigger("start_import");
+      });
+    }
+  },
 
-// frappe.ui.form.on("Supplier Catalog", {
-// 	refresh(frm) {
-
-// 	},
-// });
-
+  start_import(frm) {
+    frappe.call({
+      method: "suppliercatalog.suppliercatalog.doctype.supplier_catalog.supplier_catalog.import_bnn_items_from_file",
+      args: { docname: frm.doc.name },
+      freeze: true,
+      callback(r) {
+        if (r.message) frappe.msgprint(r.message);
+      }
+    });
+  }
+});
--- suppliercatalog/suppliercatalog/doctype/supplier_catalog/supplier_catalog.py
+++ suppliercatalog/suppliercatalog/doctype/supplier_catalog/supplier_catalog.py
@@ -1,13 +1,15 @@
 # Copyright (c) 2025, Gärtnerei Berger and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
-
-
-
 
 class SupplierCatalog(Document):
 	pass
 
-
+@frappe.whitelist()
+def import_bnn_items_from_file(docname):
+    from suppliercatalog.utils.bnn_importer import import_bnn_items_from_csv
+    doc = frappe.get_doc("Supplier Catalog", docname)
+    return import_bnn_items_from_csv(doc)
