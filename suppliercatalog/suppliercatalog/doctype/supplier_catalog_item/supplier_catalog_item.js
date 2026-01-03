d6a5e14490dcb0f6603f1cea77b279215b44cdcf
enerschnutz <ich@gaertnerei-berger.de>
2026-01-03 18:27:53 +0100

    Added custom Button

--- suppliercatalog/suppliercatalog/doctype/supplier_catalog_item/supplier_catalog_item.js
+++ suppliercatalog/suppliercatalog/doctype/supplier_catalog_item/supplier_catalog_item.js
@@ -6,3 +6,25 @@
 
 // 	},
 // });
+
+frappe.ui.form.on('Supplier Catalog Item', {
+    refresh(frm) {
+        frm.add_custom_button(__('Import Item in ERP'), function () {
+            frappe.show_alert(
+                { message: "Import started…", indicator: "blue" },
+                5
+            );
+
+            frappe.call({
+                method: "suppliercatalog.utils.sup_item_import.import_sci",
+                args: {
+                    supplier_catalog_item_names: JSON.stringify([frm.doc.name])
+                },
+                callback: function () {
+                    frappe.msgprint("Import finished.");
+                    frm.reload_doc();
+                }
+            });
+        });
+    }
+});
