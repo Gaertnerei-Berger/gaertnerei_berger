d3efa0f590b7f6f260053fb467d38dbb3db15f55
enerschnutz <ich@gaertnerei-berger.de>
2026-02-04 18:03:05 +0100

    added needed Fields and Doctype for Reseller Pricelist

---
+++ suppliercatalog/german_accounting/doctype/reseller_pricelist_settings/reseller_pricelist_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Gärtnerei Berger and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ResellerPricelistSettings(Document):
+	pass
